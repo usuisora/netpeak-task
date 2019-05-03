9e3af37be76178dd4a986d2b3dda7c44e3af4171
Ivan <pokarayano@gmail.com>
2019-05-03 16:11:20 +0300

    set2

--- src/App.js
+++ src/App.js
@@ -1,67 +1,18 @@
 import React, { createElement,useState } from 'react';
- import  img  from   './background.png'
+import Block1 from './Block1'
+ 
 
-const b1Coords = {
-  width : 500,
-  height: 500
-} 
-const b2Coords = {
-  marginX: 81,
-  marginY: 61,
-  width: 200,
-  height: 100
+const mainStyle = {
+ fontFamily: 'Roboto, Nanito',
+ backgroundColor: '#ccc',
+ height: '100vh'
 }
-
-const style = {
-  width: `${b1Coords.width}px`,
-  height:  `${b1Coords.height}px`,
-  color: 'red',
-  border: 'red solid 1px',
-  backgroundImage:  `url(${img})`,
-}
-
-const style2 = {
-  position:'absolute',
-  width:  `${b2Coords.width}px`,
-  height:  `${b2Coords.height}px`,
-  border: 'green solid 1px',
-  top:`${b2Coords.marginY}px`,
-  left:` ${b2Coords.marginX}px`,
-  resize:'none',
-  padding:0,
-  margin:0,
-  fontWeight: fontWeight
-}
-
-const boldStyle = {
-  position:'absolute',
-  top:`${b2Coords.marginY}px`,
-  left:`${b2Coords.marginX+b2Coords.width}px`,
-  height:  `${b2Coords.height}px`,
-  margin:0
-}
-
-const handleBold =()=>{
- fontWeight = (fontWeight == 200) ? 900:200 
-}
-
-const BoldButton = () => 
-            <button style ={boldStyle} onClick={handleBold} >Bold</button>
-
-  const Block2   = () => 
-  <textarea style={style2}>
-  </textarea>
-const Block1   = () => 
-    <div style={style}>
-        <Block2/>
-        <BoldButton/>
-      </div>
 function App() {
-  const [fontWeight, setfontWeight] = useState();
+  const [fontWeight, setFontWeight] = useState(200);
   
   return (
-    <div className="App">
-      <Container/>
+    <div className="App" style = {mainStyle}>
+      <Block1 fontWeight={fontWeight} setFontWeight={setFontWeight}/>
     </div>
   );
 }
---
+++ src/Block1.js
@@ -0,0 +1,83 @@
+import React,{useState} from 'react'
+import  img  from   './background.png'
+
+const b1Coords = {
+  width : 500,
+  height: 500
+} 
+const b2Coords = {
+  marginX: 81,
+  marginY: 61,
+  width: 200,
+  height: 100
+}
+
+const BoldButton = ({fontWeight,setFontWeight,displayButtom}) => {
+  const boldStyle = {
+    position:'absolute',
+    top:`${b2Coords.marginY}px`,
+    left:`${b2Coords.marginX+b2Coords.width}px`,
+    height:  `${b2Coords.height}px`,
+    margin:0,
+    display: displayButtom
+
+  }
+      return(
+        <button style ={boldStyle} 
+                                      onClick={()=>{ 
+                                                  fontWeight === 200 ? setFontWeight(900) : 
+                                                     setFontWeight(200)
+              }} >Bold</button>
+      )
+}
+            
+            
+
+  const Block2   = ({fontWeight,setDisplayButton}) => {
+        const style2 = {
+            position:'absolute',
+            width:  `${b2Coords.width}px`,
+            height:  `${b2Coords.height}px`,
+            top:`${b2Coords.marginY}px`,
+            left:` ${b2Coords.marginX}px`,
+            resize:'none',
+            padding:0,
+            margin:0,
+            fontSize:'150%',
+            fontWeight: fontWeight
+          }
+
+          const handleBlur = ({target:{value}},)=>{
+            if(value === '')
+                  setDisplayButton('none')
+          }
+          return(
+            <textarea style={style2} onFocus ={()=>{setDisplayButton('inherit')}} 
+                                      onBlur = {(e)=>{handleBlur(e)}}>
+            </textarea>
+          )
+  }
+ 
+
+
+const Block1   = ({fontWeight,setFontWeight}) => {
+    const style = {
+        width: `${b1Coords.width}px`,
+        height:  `${b1Coords.height}px`,
+        color: 'red',
+        backgroundImage:  `url(${img})`,
+      }
+    const [displayButton, setDisplayButton] = useState('none');
+      
+      return(
+             <div style={style}>
+                <Block2 fontWeight={fontWeight} setDisplayButton={setDisplayButton}  />
+                <BoldButton fontWeight={fontWeight} setFontWeight={setFontWeight} displayButtom={displayButton}  />
+            </div>
+      )
+}
+
+ 
+
+
+export default Block1
